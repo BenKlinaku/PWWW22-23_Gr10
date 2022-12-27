1f9984d0807c17606919cc2d174ae8a15c83d90b
BenKlinakuUP <ben.klinaku@student.uni-pr.edu>
2022-12-27 10:36:52 +0100

    Fixed news and video section, started two sections

--- js/VideoButton.js
+++ js/VideoButton.js
@@ -1,11 +1,19 @@
+// Button, image, text and video which need to be interacted with
 var button = document.querySelector("#vidButton");
-var image = document.querySelector("#Video img")
+var image = document.querySelector("#Video img");
+var text = document.querySelector("#textVideo");
 var video = document.querySelector("#vid");
 
+// Make button disappear content and make video appear
 button.addEventListener("click", function(){
+    image.style.display = "none";
+    text.style.display = "none";
     video.style.display = "block";
 });
 
+// Same thing but with the play button
 image.addEventListener("click", function(){
+    image.style.display = "none";
+    text.style.display = "none";
     video.style.display = "block";
 });
